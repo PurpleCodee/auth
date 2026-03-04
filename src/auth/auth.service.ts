606af1565dec7c87c08f397d2f764519a5cfbc24
Enya <enyaglf.2000@gmail.com>
2026-03-04 13:00:35 +0100

    creo el .env y instalo librerias

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,7 +22,8 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.username, sub: user.userId };
     return {
-      access_token: this.jwtService.sign(payload),//Me devuelve el token de acceso con nombre de usuario y ID
+      access_token: this.jwtService.sign(payload),
+      //Me devuelve el token de acceso con nombre de usuario y ID
     };
   }
 }
